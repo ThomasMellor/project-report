091c1f0c39dbb089061429139b7220203a401c39
ThomasMellor <tom.mel@hotmail.co.uk>
2018-03-17 10:57:14 +0000

    stuff

---
+++ gL_run_overlaid_tex2.py
@@ -0,0 +1,78 @@
+import plotting_functions as PF                                                                               
+import glob                                                                                                   
+import re                                                                                                     
+import os                                                                                                     
+import operator        
+import fnmatch 
+
+matches = []
+for root, dirnames, filenames in os.walk('./gL_run_cL_two'):
+    for filename in fnmatch.filter(filenames, '*.txt'):
+        matches.append(os.path.join(root, filename))
+counter=0
+
+lam = [[0,0],[0.2,0.2],[0.2,-0.2],[0.4,0.4],[0.4,-0.4],[0.6,-0.6],[0.6,0.6],[0.8,0.8],[0.8,-0.8],[1,1],[1,-1]]
+
+colours = {40: 'red', 64: 'blue', 48: 'green', 128: 'orange', 72: 'gray', 104: 'black', 80: 'purple'}
+sizes = [40,48, 64, 128, 72, 104,80 ]
+dic = dict() 
+
+
+for item in matches:
+    print item 
+for lambdas in lam:
+    print lambdas
+size_bool=False
+with open('./gL_run_overlaid_2.tex','w') as main_file:
+    with open('../project_code/tex_files/preamble.tex','r') as preable:
+        for line in preable:
+            main_file.write(line)
+    prev_lam = 0
+    for lambdas in lam:
+        for path_file in matches:
+            result = re.findall(r"-?\d+\.\d+|-?\d+", path_file)
+            N=result[0]
+	    dt=result[1]
+ 	    if(float(dt) != 0.01):
+		continue
+            if int(N) not in sizes:
+                continue
+            Lx=result[2]
+            Ly=result[3]
+            if float(Lx) != lambdas[0] or float(Ly) != lambdas[1]:
+                continue
+            cL=result[5]
+            if float(cL)!= 0.2:
+                continue
+            itera=result[6]
+            
+            if(prev_lam != lambdas):
+                if(size_bool == True):
+                    with open('../project_code/tex_files/end.tex','r') as end:
+                        for line in end:
+                            main_file.write(line)
+                else:
+                    size_bool = True
+                counter+=1
+            
+                if(counter==3):
+                    main_file.write('\\clearpage\n')
+                    counter=0
+                with open('../project_code/tex_files/begin.tex','r') as begin:
+                    for line in begin:
+                        main_file.write(line) 
+                    with open('./body_run.tex', 'r') as body:
+                        for line in body:
+                            main_file.write(line) 
+            add_plot =  '\t\t\\addplot[only marks, mark=o, color= ' + colours[int(N)] + ', error bars/.cd, y dir = both, y explicit]\n'
+            main_file.write(add_plot)
+            table='\ttable[y error index=2]{' + path_file + '};\n'
+            main_file.write(table)
+            legend = '\\addlegendentry{$L=' + N +'$.}\n'  
+            main_file.write(legend)
+            prev_lam = lambdas 
+    with open('../project_code/tex_files/end.tex','r') as end:
+        for line in end:
+            main_file.write(line)
+    
+    main_file.write('\\end{document}')
---
+++ plotting_functions.py
@@ -0,0 +1,72 @@
+import math
+
+def graph_points(file_name):
+    graph = []
+    with open(file_name,'r') as doc:                         
+        for line in doc.readlines():
+            l = line.strip().split(' ')
+            graph.append([float(l[0]), float(l[1])])
+    return graph 
+
+def write_file(file_name, data):
+    with open(file_name,'w') as doc:
+        for l in data:
+            doc.write(str(l[0]))
+            doc.write(' ')
+            doc.write(str(l[1]))
+            doc.write('\n')
+
+def append_file(file_name, data):
+    with open(file_name, 'a+') as doc:
+        for l in data:
+            for i in range(len(l)):
+	        doc.write(str(l[i]))                                                                             
+                doc.write(' ')
+	    doc.write('\n')                                                                                   
+			
+def error_graph(mag2_graph, mag4_graph, N) :
+    error_graph = []
+    for i in range(len(mag2_graph)) :
+	error_mag = mag4_graph[i][1]-pow(mag2_graph[i][1],2)
+	err = 2*math.sqrt(float(4)/(N-1)*error_mag*(mag4_graph[i][1]/pow(mag2_graph[i][1],4)  + pow(mag4_graph[i][1],2)/pow(mag2_graph[i][1],6)-2*mag4_graph[i][1]/pow(mag2_graph[i][1],4)))
+	error_graph.append([mag2_graph[i][0], err])
+    return error_graph 	
+
+
+def normalise(f1, f2, graph):
+    for point in graph:
+        point[0] = f1(point[0])
+        point[1] = f2(point[1])
+    return graph
+
+def div_log(t):
+    return t/math.log(t)
+
+def log_div_log(t):
+    return math.log(div_log(t))
+
+def subset_graph(x1, x2, graph):
+    subset = []
+    for point in graph:
+        if point[0] > x1 and point[0] < x2:
+            subset.append(point)
+    return subset
+
+def split_graph(first, second, graph):
+    for point in graph:
+        first.append(point[0])
+        second.append(point[1])
+
+def gr_n(graph, n):
+    gr_n_graph = []
+    for point in graph:
+        if point[0] > n:
+            gr_n_graph.append(point)
+    return gr_n_graph
+
+def gr_n_2(graph, n):
+    gr_n_graph = []
+    for point in graph:
+        if point[1] > n:
+            gr_n_graph.append(point)
+    return gr_n_graph
